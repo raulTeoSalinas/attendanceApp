e16a36812203ea9baeb161698308fe4e2c641ac7
raulTeoSalinas <raul.teo.salinas@cic.unam.mx>
2023-12-08 02:29:37 -0600

    Proyect Created. First commit

---
+++ ProyectoFinal/ProyectoFinal/ProyectoFinalApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProyectoFinalApp.swift
+//  ProyectoFinal
+//
+//  Created by Raul Salinas on 12/8/23.
+//
+
+import SwiftUI
+
+@main
+struct ProyectoFinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
